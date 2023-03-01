be4c11cdbb575053d437d5a4f43c602641b71dba
Robert Gorz <rjgorz@gmail.com>
2023-03-01 08:07:53 -0600

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+        split_word = [letter for letter in self.word]
+        for word in words:
+            if sorted(split_word) == sorted([letter for letter in word]):
+                matches.append(word)
+        return matches
